3c90ecf8f039092175b1841cc1dda124afde8be6
MATT-PC\matt <info@greenkey.ru>
2018-12-26 18:57:57 +0300

    get all categories for module

--- catalog/controller/extension/module/category.php
+++ catalog/controller/extension/module/category.php
@@ -25,39 +25,76 @@ class ControllerExtensionModuleCategory extends Controller {
 
 		$this->load->model('catalog/product');
 
-		$data['categories'] = array();
+        /*$data['categories'] = array();
 
-		$categories = $this->model_catalog_category->getCategories(0);
+        $categories = $this->model_catalog_category->getCategories(0);
 
-		foreach ($categories as $category) {
-			$children_data = array();
+        foreach ($categories as $category) {
+            $children_data = array();
 
-			if ($category['category_id'] == $data['category_id']) {
-				$children = $this->model_catalog_category->getCategories($category['category_id']);
+            if ($category['category_id'] == $data['category_id']) {
+                $children = $this->model_catalog_category->getCategories($category['category_id']);
 
-				foreach($children as $child) {
-					$filter_data = array('filter_category_id' => $child['category_id'], 'filter_sub_category' => true);
+                foreach($children as $child) {
+                    $filter_data = array('filter_category_id' => $child['category_id'], 'filter_sub_category' => true);
 
-					$children_data[] = array(
-						'category_id' => $child['category_id'],
-						'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-						'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
-					);
-				}
-			}
+                    $children_data[] = array(
+                        'category_id' => $child['category_id'],
+                        'name' => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                        'href' => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+                    );
+                }
+            }
 
-			$filter_data = array(
-				'filter_category_id'  => $category['category_id'],
-				'filter_sub_category' => true
-			);
+            $filter_data = array(
+                'filter_category_id'  => $category['category_id'],
+                'filter_sub_category' => true
+            );
 
-			$data['categories'][] = array(
-				'category_id' => $category['category_id'],
-				'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
-				'children'    => $children_data,
-				'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
-			);
-		}
+            $data['categories'][] = array(
+                'category_id' => $category['category_id'],
+                'name'        => $category['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                'children'    => $children_data,
+                'href'        => $this->url->link('product/category', 'path=' . $category['category_id'])
+            );
+        }*/
+
+        $allcategories = $this->model_catalog_category->getAllCategories();
+
+        function form_tree($mess)
+        {
+            if (!is_array($mess)) {
+                return false;
+            }
+            $tree = array();
+            foreach ($mess as $value) {
+                $tree[$value['parent_id']][] = $value;
+            }
+            return $tree;
+        }
+
+        //$parent_id - какой parentid считать корневым
+        //по умолчанию 0 (корень)
+        function build_tree($cats, $parent_id)
+        {
+            if (is_array($cats) && isset($cats[$parent_id])) {
+                $tree = array();
+                foreach ($cats[$parent_id] as $cat) {
+                    $tree[] = array(
+                        'category_id' => $cat['category_id'],
+                        'name'        => $cat['name'],
+                        'href'        => $this->url->link('product/category', 'path=' . $cat['category_id']),
+                        'children'    => build_tree($cats, $cat['category_id'])
+                    );
+                }
+            } else {
+                return false;
+            }
+            return $tree;
+        }
+
+        $data = form_tree($allcategories);
+        $data = build_tree($data, 0);
 
 		return $this->load->view('extension/module/category', $data);
 	}
